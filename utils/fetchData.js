383abf7a9ee28d4fab0c251522be3d0c06a29c0a
MahmoudKamal01 <dddd>
2022-08-17 04:03:29 +0200

    Update fetchData.js

--- utils/fetchData.js
+++ utils/fetchData.js
@@ -3,23 +3,14 @@ const baseUrl = process.env.BASE_URL;
 export const getData = async (url, token) => {
   const res = await fetch(`${baseUrl}/api/${url}`, {
     method: "GET",
-    headers: [
-      { Authorization: token },
-      { key: "Access-Control-Allow-Credentials", value: "true" },
-      {
-        key: "Access-Control-Allow-Origin",
-        value: "https://hotch-potch.herokuapp.com",
-      },
-      {
-        key: "Access-Control-Allow-Methods",
-        value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
-      },
-      {
-        key: "Access-Control-Allow-Headers",
-        value:
-          "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-      },
-    ],
+    headers: {
+      Authorization: token,
+      "Access-Control-Allow-Credentials": "true",
+      "Access-Control-Allow-Origin": "https://hotch-potch.herokuapp.com",
+      "Access-Control-Allow-Methods": "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+      "Access-Control-Allow-Headers":
+        "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+    },
   });
 
   const data = await res.json();
@@ -32,6 +23,11 @@ export const postData = async (url, post, token) => {
     headers: {
       "Content-Type": "application/json",
       Authorization: token,
+      "Access-Control-Allow-Credentials": "true",
+      "Access-Control-Allow-Origin": "https://hotch-potch.herokuapp.com",
+      "Access-Control-Allow-Methods": "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+      "Access-Control-Allow-Headers":
+        "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
     },
     body: JSON.stringify(post),
   });
@@ -46,6 +42,11 @@ export const putData = async (url, post, token) => {
     headers: {
       "Content-Type": "application/json",
       Authorization: token,
+      "Access-Control-Allow-Credentials": "true",
+      "Access-Control-Allow-Origin": "https://hotch-potch.herokuapp.com",
+      "Access-Control-Allow-Methods": "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+      "Access-Control-Allow-Headers":
+        "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
     },
     body: JSON.stringify(post),
   });
@@ -60,6 +61,11 @@ export const patchData = async (url, post, token) => {
     headers: {
       "Content-Type": "application/json",
       Authorization: token,
+      "Access-Control-Allow-Credentials": "true",
+      "Access-Control-Allow-Origin": "https://hotch-potch.herokuapp.com",
+      "Access-Control-Allow-Methods": "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+      "Access-Control-Allow-Headers":
+        "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
     },
     body: JSON.stringify(post),
   });
@@ -74,6 +80,11 @@ export const deleteData = async (url, token) => {
     headers: {
       "Content-Type": "application/json",
       Authorization: token,
+      "Access-Control-Allow-Credentials": "true",
+      "Access-Control-Allow-Origin": "https://hotch-potch.herokuapp.com",
+      "Access-Control-Allow-Methods": "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+      "Access-Control-Allow-Headers":
+        "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
     },
   });
 
